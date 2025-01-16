cc697cd858d4117c5ef33d7fdf66cc2fc496f034
FISHBOOK <1031500172@qq.com>
2025-01-16 11:57:44 +0800

    build the wecaht code

--- main.js
+++ main.js
@@ -28,3 +28,32 @@ document.addEventListener("DOMContentLoaded", function () {
   window.addEventListener("resize", handleResize);
 });
 
+
+
+// 获取所有的按钮和弹窗元素
+const openModalBtns = document.querySelectorAll('.open-modal-btn');
+const modals = document.querySelectorAll('.modal');
+const closeBtns = document.querySelectorAll('.modal-close');
+
+// 显示弹窗
+openModalBtns.forEach((btn, index) => {
+  btn.onclick = function() {
+    modals[index].style.display = 'flex'; // 根据按钮点击显示对应的弹窗
+  };
+});
+
+// 关闭弹窗
+closeBtns.forEach((closeBtn, index) => {
+  closeBtn.onclick = function() {
+    modals[index].style.display = 'none'; // 隐藏对应的弹窗
+  };
+});
+
+// 点击弹窗外部区域时关闭弹窗
+window.onclick = function(event) {
+  modals.forEach((modal, index) => {
+    if (event.target === modal) {
+      modal.style.display = 'none'; // 隐藏弹窗
+    }
+  });
+}
